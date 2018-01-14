08c4edc1159dc9a6b4bdb7b7ceccbb377419e767
god <164798848@qq.com>
2018-01-14 19:55:08 +0800

    code

--- errors.go
+++ errors.go
@@ -7,13 +7,11 @@ import (
 	"net/http"
 )
 
-const ERROR_UNKNOWN = "UnknownError"
-const ERROR_UNAUTHORIZED = "Unauthorized"
-const ERROR_NOT_FOUND = "NotFound"
-const ERROR_INTERNAL = "ServerInternalError"
-const ERROR_INVALID_PARAMS = "InvalidParams"
-const ERROR_INTERNAL_EXCEPTION = "InternalException"
-const ERROR_ALREADY_EXISTS = "AlreadyExists"
+const ErrUnknown = "Unknown"
+const ErrUnauthorized = "Unauthorized"
+const ErrNotFound = "NotFound"
+const ErrInvalidParams = "InvalidParams"
+const ErrAlreadyExists = "AlreadyExists"
 
 type ParamError struct {
 	Field   string `json:"field,omitempty"`
@@ -43,10 +41,6 @@ func (e *Error) Error() string {
 	return string(s)
 }
 
-func Unknown(message string) *Error {
-	return &Error{Status: http.StatusInternalServerError, Code: ERROR_UNKNOWN, Message: message}
-}
-
 func Wrap(err interface{}) *Error {
 	if err == nil {
 		panic("err is nil")
@@ -58,27 +52,31 @@ func Wrap(err interface{}) *Error {
 	case error:
 		return &Error{
 			Status:  http.StatusInternalServerError,
-			Code:    ERROR_UNKNOWN,
+			Code:    ErrUnknown,
 			Message: err.(error).Error(),
 		}
 	default:
 		return &Error{
 			Status:  http.StatusInternalServerError,
-			Code:    ERROR_UNKNOWN,
+			Code:    ErrUnknown,
 			Message: fmt.Sprint(err),
 		}
 	}
 }
 
+func Unknown(message string) *Error {
+	return &Error{Status: http.StatusInternalServerError, Code: ErrUnknown, Message: message}
+}
+
 func InvalidParams(params ...*ParamError) *Error {
-	return &Error{Status: http.StatusBadRequest, Code: ERROR_INVALID_PARAMS, Errors: params}
+	return &Error{Status: http.StatusBadRequest, Code: ErrInvalidParams, Errors: params}
 }
 
 func InvalidParam(field string, message string) *Error {
 	return &Error{
 		Status: http.StatusBadRequest,
-		Code:   ERROR_INVALID_PARAMS,
-		Errors: []*ParamError{{Field: field, Code: ERROR_INVALID_PARAMS, Message: message}}}
+		Code:   ErrInvalidParams,
+		Errors: []*ParamError{{Field: field, Code: ErrInvalidParams, Message: message}}}
 }
 
 func BadRequest(code string, message string) *Error {
@@ -86,17 +84,13 @@ func BadRequest(code string, message string) *Error {
 }
 
 func Unauthorized(message string) *Error {
-	return &Error{Status: http.StatusUnauthorized, Code: ERROR_UNAUTHORIZED, Message: message}
+	return &Error{Status: http.StatusUnauthorized, Code: ErrUnauthorized, Message: message}
 }
 
 func NotFound(message string) *Error {
-	return &Error{Status: http.StatusNotFound, Code: ERROR_NOT_FOUND, Message: message}
+	return &Error{Status: http.StatusNotFound, Code: ErrNotFound, Message: message}
 }
 
 func AlreadyExists(message string) *Error {
-	return &Error{Status: http.StatusBadRequest, Code: ERROR_ALREADY_EXISTS, Message: message}
-}
-
-func InternalServerError(message string) *Error {
-	return &Error{Status: http.StatusInternalServerError, Code: ERROR_INTERNAL, Message: message}
+	return &Error{Status: http.StatusBadRequest, Code: ErrAlreadyExists, Message: message}
 }
